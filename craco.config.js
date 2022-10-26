9e7843bb43fd9e4a8286070fd6c0bd34aaf0f9ae
Etcetera <yilonglei790@gmail.com>
2022-10-26 16:58:28 +0800

    项目初始化

---
+++ craco.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+// const CracoLessPlugin = require('craco-less')
+
+const resolve = (dir) => path.resolve(__dirname, dir)
+
+module.exports = {
+  // plugins: [{ plugin: CracoLessPlugin }],
+  webpack: {
+    alias: {
+      '@': resolve('src')
+    }
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +14,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
