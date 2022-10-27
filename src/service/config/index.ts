3750ed9c8de6415bfeef24260c04900e2b59ae62
Etcetera <yilonglei790@gmail.com>
2022-10-27 21:01:32 +0800

    封装axios请求库

--- src/service/config/index.ts
+++ src/service/config/index.ts
@@ -1,27 +1,18 @@
-// 1.区分开发环境和生产环境
-// export const BASE_URL = 'http://coderwhy.dev:8000'
-// export const BASE_URL = 'http://codercba.prod:8000'
-
-// 2.代码逻辑判断, 判断当前环境
-// vite默认提供的环境变量
-// console.log(import.meta.env.MODE)
-// console.log(import.meta.env.DEV) // 是否开发环境
-// console.log(import.meta.env.PROD) // 是否生产环境
-// console.log(import.meta.env.SSR) // 是否是服务器端渲染(server side render)
-
-let BASE_URL = ""
-if (import.meta.env.PROD) {
-  // 生产环境
-  BASE_URL = "http://152.136.185.210:4000"
-} else {
-  // 开发环境
-  BASE_URL = "http://152.136.185.210:5000"
-}
-
-// console.log(BASE_URL)
-
-// 3.通过创建.env文件直接创建变量
-// console.log(import.meta.env.VITE_URL)
-
+// 1.手动切换
+export const BASE_URL = 'http://codercba.com:9002'
+// export const BASE_URL = 'http://codercba.prod:9002'
 export const TIME_OUT = 10000
-export { BASE_URL }
+
+// 2.依赖当前环境: development/production
+// console.log(process.env.NODE_ENV)
+// let BASE_URL = ''
+// if (process.env.NODE_ENV === 'development') {
+//   BASE_URL = 'http://codercba.dev:9002'
+// } else {
+//   BASE_URL = 'http://codercba.prod:9002'
+// }
+
+// export { BASE_URL }
+
+// 3.从定义的环境变量的配置文件中, 加载变量
+// console.log(process.env.REACT_APP_BASE_URL)
