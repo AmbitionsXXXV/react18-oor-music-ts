ea785dd79057567ad07a450b8b90f3d17b079d6b
Etcetera <yilonglei790@gmail.com>
2022-10-31 15:35:02 +0800

    AppFooter模块

--- src/components/app-footer/index.tsx
+++ src/components/app-footer/index.tsx
@@ -1,5 +1,7 @@
-import React, { memo } from 'react'
+import React, { Fragment, memo } from 'react'
 import type { ReactNode, FC } from 'react'
+import { AppFooterWrapper, FooterLeft, FooterRight } from './style'
+import { footerImages, footerLinks } from '@/assets/data/local_data'
 
 interface IProps {
   children?: ReactNode
@@ -7,9 +9,77 @@ interface IProps {
 
 const AppFooter: FC<IProps> = () => {
   return (
-    <div>
-      <h2>AppFooter 组件</h2>
-    </div>
+    <AppFooterWrapper>
+      <div className="wrap-v2 content">
+        <FooterLeft className="left">
+          <div className="link">
+            {footerLinks.map((item) => {
+              return (
+                <Fragment key={item.title}>
+                  <a href={item.link} target="_blank" rel="noopener noreferrer">
+                    {item.title}
+                  </a>
+                  <span className="line">|</span>
+                </Fragment>
+              )
+            })}
+          </div>
+          <div className="copyright">
+            <span>网易公司版权所有©1997-2020</span>
+            <span>
+              杭州乐读科技有限公司运营：
+              <a
+                href="https://p1.music.126.net/Mos9LTpl6kYt6YTutA6gjg==/109951164248627501.png"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                浙网文[2018]3506-263号
+              </a>
+            </span>
+          </div>
+          <div className="report">
+            <span>违法和不良信息举报电话：0571-89853516</span>
+            <span>
+              举报邮箱：
+              <a
+                href="mailto:ncm5990@163.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ncm5990@163.com
+              </a>
+            </span>
+          </div>
+          <div className="info">
+            <span>粤B2-20090191-18</span>
+            <a
+              href="http://www.beian.miit.gov.cn/publish/query/indexFirst.action"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              工业和信息化部备案管理系统网站
+            </a>
+          </div>
+        </FooterLeft>
+        <FooterRight className="right">
+          {footerImages.map((item, index) => {
+            return (
+              <li className="item" key={item.link}>
+                <a
+                  className="link"
+                  href={item.link}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  {' '}
+                </a>
+                <span className="title">{item.title}</span>
+              </li>
+            )
+          })}
+        </FooterRight>
+      </div>
+    </AppFooterWrapper>
   )
 }
 
