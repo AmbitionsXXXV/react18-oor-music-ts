c6f5b748e66e72106b48da2c1ff153fd73b53093
Etcetera <yilonglei790@gmail.com>
2022-10-31 11:12:05 +0800

    播放器/歌词模块

--- src/views/discover/child-views/recommend/index.tsx
+++ src/views/discover/child-views/recommend/index.tsx
@@ -13,6 +13,9 @@ import { RecommendWrapper } from './style'
 import HotRecommend from './c-cpns/hot-recommend'
 import NewAlbum from './c-cpns/new-album'
 import TopRanking from './c-cpns/top-ranking'
+import UserLogin from './c-cpns/user-login'
+import SettleSinger from './c-cpns/settle-singer'
+import HotAnchor from './c-cpns/hot-anchor'
 
 interface IProps {
   children?: ReactNode
@@ -39,7 +42,11 @@ const Recommend: FC<IProps> = () => {
           <NewAlbum />
           <TopRanking />
         </div>
-        <div className="right">right</div>
+        <div className="right">
+          <UserLogin />
+          <SettleSinger />
+          <HotAnchor />
+        </div>
       </div>
     </RecommendWrapper>
   )
