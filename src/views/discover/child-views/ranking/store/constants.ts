cc3dbe97ad855a6ca9afc89722e3e93a263442b3
Etcetera <yilonglei790@gmail.com>
2022-10-31 17:49:04 +0800

    排行榜页面搭建

--- src/views/discover/child-views/ranking/store/constants.ts
+++
@@ -1,3 +0,0 @@
-export const CHANGE_TOP_LIST = 'ranking/CHANGE_TOP_LIST'
-export const CHANGE_CURRENT_INDEX = 'rangking/CHANGE_CURRENT_INDEX'
-export const CHANGE_PLAY_LIST = 'ranking/CHANGE_PLAY_LIST'
